0f1e6c9f88c5cde01ba3ae42ae42a1bce1ccc916
Rekunan <Mangsn05@protonmail.com>
2024-04-08 18:11:57 -0500

    April 8, 2024

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -191,6 +191,13 @@
 
   programs.gamemode.enable = true;
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+
   home-manager = {
     # also pass inputs to home-manager modules
     extraSpecialArgs = {inherit inputs;};
