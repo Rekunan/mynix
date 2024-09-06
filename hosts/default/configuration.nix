2dd5368d5bd69260cc876dbf10c71a907ad0cfea
Rekunan <Mangsn05@protonmail.com>
2024-09-05 22:24:18 -0500

    Commit at timestamp 1725593058

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -13,6 +13,16 @@
       ../../modules/nixos/mounts.nix
     ];
 
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+    bbswitch
+  ];
+  boot.kernelModules = [ "v4l2loopback" ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
+  
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
