17f9b30166494e850513f734af705a8557dfec05
Rekunan <Mangsn05@protonmail.com>
2024-09-05 22:45:38 -0500

    Commit at timestamp 1725594338

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -13,12 +13,7 @@
       ../../modules/nixos/mounts.nix
     ];
 
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    v4l2loopback
-    bbswitch
-    rtw88
-    rtw89
-  ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback bbswitch rtw88 rtw89 ];
   boot.kernelModules = [ "v4l2loopback" ];
   boot.extraModprobeConfig = ''
     options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
